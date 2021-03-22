e06c91b58eaa86a72661a8521195c6035d755892
Robby Michels <robby.michels@teamrockstars.nl>
2021-03-22 14:23:09 +0100

    Initial Commit

---
+++ pinch_tech_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pinch_tech_test
+//
+//  Created by Robby Michels on 22/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pinch_tech_testTests/pinch_tech_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  pinch_tech_testTests.swift
+//  pinch_tech_testTests
+//
+//  Created by Robby Michels on 22/03/2021.
+//
+
+import XCTest
+@testable import pinch_tech_test
+
+class pinch_tech_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
